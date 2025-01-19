dfa333364e52c034810b6a2a4952e92342358b92
OTIS <72382663+1domybest@users.noreply.github.com>
2025-01-19 17:08:48 +0900

    .
    .

---
+++ src/main/java/com/example/spring_noneStop_deploy/SpringNoneStopDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_noneStop_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoneStopDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNoneStopDeployApplication.class, args);
+	}
+
+}
